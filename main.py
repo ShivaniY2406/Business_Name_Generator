d0fd0e8a2d491022ee291f978d8a76be51324a6b
Shivani <shivaniyadav99837@gmail.com>
2023-07-26 22:25:53 +0530

    Initial Commit

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ import streamlit as st
 import LangChainHelper
 st.title("Business Name Generator along with services")
 
-type = st.sidebar.text_input("Enter the type of business"
-                             )
+type = st.sidebar.text_input("Enter the type of business")
 
 if type:
     response = LangChainHelper.generate_name(type)
